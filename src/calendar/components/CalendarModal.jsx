8f346f68e829ada304eb71f0bda2859b52bf67d8
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-03-09 21:50:38 -0400

    añadir un evento

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -28,7 +28,7 @@ Modal.setAppElement('#root');
 export const CalendarModal = () => {
 
     const { isDateModalOpen, closeDateModal } = useUiStore();
-    const { activeEvent } = useCalendarStore();
+    const { activeEvent, startSavingEvent } = useCalendarStore();
     const [formSubmitted, setFormSubmitted] = useState(false);
 
     const [formValues, setFormValues] = useState({
@@ -69,7 +69,7 @@ export const CalendarModal = () => {
      })
   }
 
-  const onSubmit = ( event ) =>{
+  const onSubmit = async( event ) =>{
       event.preventDefault();
       setFormSubmitted(true);
 
@@ -82,6 +82,10 @@ export const CalendarModal = () => {
       }
 
       if( formValues.title.length <= 0 ) return;
+
+      await startSavingEvent( formValues );
+      closeDateModal();
+      setFormSubmitted(false);
   }
     
     
