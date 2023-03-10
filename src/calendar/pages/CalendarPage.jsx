3eebc17d581792c2c060447ca44eed7063e1acce
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-03-09 22:14:30 -0400

    delete

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { Navbar, CalendarEvent, CalendarModal, FabAddNew } from '../';
+import { Navbar, CalendarEvent, CalendarModal, FabAddNew, FabDelete } from '../';
 
 import { localizer, getMessagesEs } from '../../helpers';
 import { useUiStore,useCalendarStore } from '../../hooks';
@@ -58,6 +58,7 @@ export const CalendarPage = () => {
       />
       <CalendarModal />
       <FabAddNew />
+      <FabDelete />
     </>
   )
 }
